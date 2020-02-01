e88e9ac8831f5fa4a09c0ef8032d25661066e22e
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-01 10:20:35 -0600

    Added Component Base Wireframes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//Import Components
+import Details from '../Details/Details'
+import Edit from '../Edit/Edit'
+import List from '../List'
+
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
       </div>
     );
   }
---
+++ src/components/Edit/Edit.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class Edit extends Component{
+ 
+    render(){
+        return(
+        <div>
+            I am Edit Component
+        </div>
+        )
+    
+    }
+}
+export default Edit

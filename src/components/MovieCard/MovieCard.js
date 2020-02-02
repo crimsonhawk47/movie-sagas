7597fd9f1b69133016384da471041aaefa57fdb3
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-02 13:53:29 -0600

    conditional genre rendering on MovieCard

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import {withRouter} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 class MovieCard extends Component {
 
     goToDetailsPage = (movieID) => {
         console.log(`Heyooo my id is ${movieID}`);
-        this.props.dispatch({type: 'SET_CURRENT_MOVIE', payload: movieID})
+        this.props.dispatch({ type: 'SET_CURRENT_MOVIE', payload: movieID })
         this.props.history.push('/details')
     }
 
@@ -14,12 +14,21 @@ class MovieCard extends Component {
         let movieID = this.props.id
         let movieIndex = movieID - 1
         let movie = this.props.reduxStore.movies[movieIndex]
+        let genreDisplay;
+        if (this.props.showGenres) {
+            genreDisplay = <ul>
+                {movie.genres.map((genre, index) => {
+                    return <li key={index}>{genre}</li>
+                })}
+            </ul>
+        }
 
         return (
             <div>
-                    <p>{movie.title}</p>
-                    <img onClick={() => {this.goToDetailsPage(movieID)}} src={movie.poster} />
-                    <p>{movie.description}</p>
+                <p>{movie.title}</p>
+                <img onClick={() => { this.goToDetailsPage(movieID) }} src={movie.poster} />
+                <p>{movie.description}</p>
+                {genreDisplay}
             </div>
 
 

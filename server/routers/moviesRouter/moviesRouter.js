d3fb07fc5c7433fd86c9068fb045a43fbda3ad57
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-02 12:45:40 -0600

    Converted SQL query to put genres in the movie reducer as well

--- server/routers/moviesRouter/moviesRouter.js
+++ server/routers/moviesRouter/moviesRouter.js
@@ -4,25 +4,39 @@ const axios = require('axios');
 const router = express.Router();
 const pool = require('../../modules/pool');
 
-router.get('/', (req,res) => {
-    let queryText = `SELECT * FROM "movies" ORDER BY "id"`
+router.get('/', (req, res) => {
+    let queryText = `SELECT "movies".id, "movies".title, "movies".description, "movies".poster, array_agg("genres".name) as "genres" FROM "movies_genres"
+                    JOIN "movies" on movies_id = "movies".id
+                    JOIN "genres" on genres_id = "genres".id
+                    GROUP BY "movies".id, "movies".title, "movies".description, "movies".poster
+                    ORDER BY "movies".id;`
     pool.query(queryText)
-    .then(response=>{
-        console.log(response.rows);
-        
-        res.send(response.rows)
-    })
-    .catch(err=>{
-        console.log(err);
-        
-    })
+        .then(response => {
+            console.log(response.rows);
+
+            res.send(response.rows)
+        })
+        .catch(err => {
+            console.log(err);
+
+        })
 })
 
-router.post('/', (req,res)=>{
-    console.log(req.body);
-    console.log(`HEYOOOO`);
+router.post('/', (req, res) => {
     res.sendStatus(200)
-    
+    let queryText = `UPDATE "movies"
+        SET "description" = 'Derp'
+        WHERE "id" = $1;`
+    pool.query(queryText, [req.body.id])
+        .then((result) => {
+            res.sendStatus(200)
+        })
+        .catch((err) => {
+            res.sendStatus(500)
+        })
+
 })
 
 module.exports = router
+
+

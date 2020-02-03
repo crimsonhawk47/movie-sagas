8a8d025be2688fd54895a06d432e68b7897482e5
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-02 23:56:31 -0600

    Updated database.sql so user can see genre dummy testing

--- database.sql
+++ database.sql
@@ -16,6 +16,14 @@ CREATE TABLE "genres" (
 -- You will need to create the junction table that stores the relationships between "movies" and "genres"
 -- This table will need to be populated with some data as well (INSERTS)
 -- Recall that this Junction Table will just be a table of ids!
+CREATE TABLE "movies_genres" (
+	"id" SERIAL PRIMARY KEY,
+	"movies_id" INT REFERENCES "movies",
+	"genres_id" INT REFERENCES "genres"
+);
+
+INSERT INTO "movies_genres" ("movies_id", "genres_id")
+VALUES (1 ,1), (1 ,6), (1 ,11), (2, 10), (2, 9), (3, 13), (3, 7), (4, 1), (4, 4), (4,2), (5, 6), (6, 8), (6, 1), (7, 1), (8, 1), (8,6), (9,2), (9, 4), (10, 12), (10, 7), (11, 11), (11, 5), (12, 6), (12, 3), (13, 5), (13, 10), (14, 2);
 
 
 
